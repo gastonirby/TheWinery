78c6b0707f1a97f958a2acbf695386c2d5c2f035
Gaston <gaston.irby@gmail.com>
2023-03-08 12:38:32 -0800

    initial set up

---
+++ app.js
@@ -0,0 +1,20 @@
+const navLinks = document.querySelectorAll('nav a')
+const imgsNav = document.querySelectorAll('nav img')
+const title = document.querySelector('h1')
+const verticalLine = document.querySelector('.middle-line')
+const grapesLogo = document.querySelector('.home-content img')
+const homeBtn = document.querySelector('.home-content button')
+
+window.addEventListener('load', revealAnim);
+
+function revealAnim() {
+    const TLFADE = gsap.timeline();
+
+    TLFADE
+    .from(title, {autoAlpha: 0, y: -50, delay: 0.2})
+    .to(verticalLine, {height: 200, duration: 1.5}, '-=0.2')
+    .from(grapesLogo, {autoAlpha: 0, y: -50, ease: 'bounce', duration: 2}, '-=0.75')
+    .from(homeBtn, {autoAlpha: 0, y: -50}, '-=0.2')
+    .from(navLinks, {autoAlpha: 0, y: -50, duration: 0.4, stagger: 0.4}, '-=0.2')
+    .from(imgsNav, {autoAlpha: 0, y: -50})
+}
